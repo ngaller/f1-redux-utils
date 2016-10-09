9decacc0213f76035bac50a538fc4fd26561e88f
Nicolas Galler <ngaller@gmail.com>
2016-10-09 05:48:52 -0400

    Add createAction0 method for action without payload

--- actionCreators.js
+++ actionCreators.js
@@ -11,3 +11,22 @@ export function createActions(...types) {
 export function createAction(type) {
   return (payload, error) => (Object.assign({ type }, payload ? { payload } : {}, error ? { error } : {}))
 }
+
+/**
+ * Create a single action of the form x => { type }
+ * Use this for actions that do not carry any payload, this is useful especially if the action is
+ * going to be used as an event handler to avoid accidentally including the DOM event as payload.
+ */
+export function createAction0(type) {
+  return () => ({ type })
+}
+
+/**
+ * Return an array of action creators.
+ * Use this for actions that do not carry any payload.
+ * 
+ * @returns {Array}
+ */
+export function creactActions0(...types) {
+  return types.map(createAction0)
+}
